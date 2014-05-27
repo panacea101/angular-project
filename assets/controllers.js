6f5596cbe0bef205a4377a90749eb3bdb25844ea
panacea101 <panacea101@gmail.com>
2014-05-27 11:56:55 -0400

    Update controllers.js

--- assets/controllers.js
+++ assets/controllers.js
@@ -1,4 +1,10 @@
-angular.module('myModule', ['ui.bootstrap']);
+angular.module('myProject', ['ui.bootstrap']);
+var TabsDemoCtrl = function ($scope) {
+  $scope.tabs = [
+    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+  ];
+
 function DropdownCtrl($scope) {
   $scope.items = [
     'The first choice!',
